01e72aa881f02ac7ad73ab829ee77bd4601d7eae
iFarruxdev <alimovf01@gmail.com>
2025-11-09 02:17:57 -0700

    add local host

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,5 +1,19 @@
-let toggleButton = document.querySelector('.site-header-toggler');
+let toggleButton = document.querySelector(".site-header-toggler");
+let toggleText = document.querySelector(".site-header-toggler-inner-text");
 
-toggleButton.addEventListener('click' , () => {
-  document.body.classList.toggle('dark-mode')
-})
+if (localStorage.getItem("theme")) {
+  document.body.classList.add("dark-mode");
+}
+
+if (toggleButton) {
+  toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList == "dark-mode") {
+      toggleText.textContent = "Light Mode"
+      localStorage.setItem('theme' , "dark-mode");
+    } else {
+      toggleText.textContent = "Dark Mode"
+      localStorage.setItem('theme' , "");
+    }
+  });
+}
